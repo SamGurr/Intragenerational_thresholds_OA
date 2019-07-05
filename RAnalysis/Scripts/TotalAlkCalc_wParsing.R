b51be0eea4d971d2d25a325cfd18f9a970fcdac1
SamJGurr <samjgurr@gmail.com>
2019-07-05 14:47:34 -0700

    minor changes for chem run on 20190703

--- RAnalysis/Scripts/TotalAlkCalc_wParsing.R
+++ RAnalysis/Scripts/TotalAlkCalc_wParsing.R
@@ -28,13 +28,13 @@ library(seacarb) #used to calculate TA
 library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
-path<-"Data/TA/20190626" #the location of all your titration files
-massfile<-"20190626mass_RUN1.csv" # name of your file with masses
-titrationfile<-'20190626_RUN1.csv'# name of the last titration file run
+path<-"Data/TA/20190703" #the location of all your titration files
+massfile<-"20190703mass_CRM.csv" # name of your file with masses
+titrationfile<-'20190703_CRM.csv'# name of the last titration file run
 
 
 # Date that the data were run
-date<-'20190626'
+date<-'20190703'
 
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
